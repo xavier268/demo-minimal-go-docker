efdba900d3161b76d24c4b5f13b5af1fd77cafe2
xavier <xavier@fed29onDellXPS>
2019-01-15 17:49:50 +0100

    Separate hnadler file

--- main.go
+++ main.go
@@ -4,40 +4,11 @@ import (
 	"fmt"
 	"log"
 	"net/http"
-	"os"
-	"time"
 )
 
 var port = 8080
 
-// defaultHandler print a message with the path information
-func defaultHandler(w http.ResponseWriter, r *http.Request) {
-	var p = r.URL.Path
-	fmt.Fprintf(w, "The path was : %s", p)
-}
-
-// timeHandler give current server time
-func timeHandler(w http.ResponseWriter, r *http.Request) {
-	tm := time.Now().Format(time.RFC1123)
-	w.Write([]byte("The time is: " + tm))
-}
-
-// stopHandler triggers a server stop 2 seconds later
-func stopHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "\nThe server will stop in 2 seconds")
-	// Using a goroutine, to allow response time to be sent ...
-	go stopLater(2 * time.Second)
-}
-
-// stopLater waits for duration before stoppping everything
-func stopLater(d time.Duration) {
-	fmt.Printf("Server will stop in %v  \n", d)
-	time.Sleep(d)
-	fmt.Println("Server stopped !")
-	os.Exit(0)
-}
-
-// define routes, creting a local ServeMux
+// define routes, creating a local ServeMux
 // For security reasons, better not to rely on global defaultMux
 func getMux() *http.ServeMux {
 	mux := http.NewServeMux()
---
+++ myHandlers.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+)
+
+// defaultHandler print a message with the path information
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	var p = r.URL.Path
+	fmt.Fprintf(w, "The path was : %s", p)
+}
+
+// timeHandler give current server time
+func timeHandler(w http.ResponseWriter, r *http.Request) {
+	tm := time.Now().Format(time.RFC1123)
+	w.Write([]byte("The time is: " + tm))
+}
+
+// stopHandler triggers a server stop 2 seconds later
+func stopHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "\nThe server will stop in 2 seconds")
+	// Using a goroutine, to allow response time to be sent ...
+	go stopLater(2 * time.Second)
+}
+
+// stopLater waits for duration before stoppping everything
+func stopLater(d time.Duration) {
+	fmt.Printf("Server will stop in %v  \n", d)
+	time.Sleep(d)
+	fmt.Println("Server stopped !")
+	os.Exit(0)
+}
